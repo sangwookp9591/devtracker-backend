ebc751ea87441026407bdb2d6e88b76bdbab3f61
sangwookpark <sangwookp9591@gmail.com>
2025-09-12 20:41:46 +0900

    chore: devtracker-backend init

---
+++ src/main/java/com/devtracker/backend/DevTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.devtracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTrackerApplication.class, args);
+	}
+
+}
